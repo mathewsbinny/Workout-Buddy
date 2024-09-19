cb337ed5f7939c896f30d048b8e542502340161b
mathewsbinny <mathewsb.00@gmail.com>
2024-09-19 02:32:03 -0400

    Added API Routes & MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 //express app
 const app = express()
 
-//routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port...')
-})
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB, listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
